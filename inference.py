842ddf2adc5d55241957a4b3f402b007bb5261e5
Black <1587754061@qq.com>
2023-03-08 11:15:11 +0800

    mnist test

---
+++ inference.py
@@ -0,0 +1,50 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/3/6 11:23
+# @Author : Black
+# @File : inference
+# @Project : mnist
+
+import torch
+from train import *
+
+class_mapping = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9"
+]
+
+
+def predict(model, input, target, class_mapping):
+    model.eval()
+    with torch.no_grad():
+        predictions = model(input)
+        # Tensor (1, 10) -> [[0.1, 0.01, ..., 0.6]]
+        predicted_index = predictions[0].argmax(0)
+        predicted = class_mapping[predicted_index]
+        expected = class_mapping[target]
+    return predicted, expected
+
+
+if __name__ == '__main__':
+    # load back the model
+    feed_forward_net = FeedForwardNet()
+    state_dict = torch.load("feedforwardnet.pth")
+    feed_forward_net.load_state_dict(state_dict)
+    # load MNIST validation dataset
+    _, validation_data = down_load_mnist_datasets()
+
+    # get a sample from the validation dataset for inference
+    input, target = validation_data[0][0], validation_data[0][1]
+
+    # make an inference
+    predicted, expected = predict(feed_forward_net, input, target,
+                                  class_mapping)
+
+    print(f"Predicted:'{predicted}, expected:'{expected}'")
